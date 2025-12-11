27f0b4e6c57732be7aaf70a22344f9ce93f187f3
mediosiglomartin <mediosiglomartin@gmail.com>
2025-12-10 23:58:34 -0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 
 
 $(document).ready(function () {
+    // Change Header Backgroun Color
     $(window).on('scroll', function () {
-        if ($(window).scrollTop() >= 300) {
-            $('.header__logo').addClass('show');
+        if ($(window).scrollTop() >= 150) {
+            $('.header').addClass('show');
+        } else if (!$('#menu').hasClass('open')) {
+            $('.header').removeClass('show');
+        }
+    });
+
+    // Open Menu on Mobile
+    $('#hamburger-menu').on('click', function () {
+        $('#menu').toggleClass('open');
+
+        if ($('.header').hasClass('show') && !$(window).scrollTop() >= 150) {
+            $('.header').removeClass('show');
         } else {
-            $('.header__logo').removeClass('show');
+            $('.header').addClass('show');
         }
     });
 });
